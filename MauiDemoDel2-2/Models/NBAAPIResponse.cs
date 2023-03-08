05879ab135933ab92e733c073d1b447fc4f02ddd
KaveCoding <teachereli@outlook.com>
2023-03-08 15:21:22 +0100

    test2

---
+++ MauiDemoDel2-2/Models/NBAAPIResponse.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MauiDemoDel2_2.Models
+{
+    internal class NBAAPIResponse
+    {
+        public string get { get; set; }
+        public Parameters parameters { get; set; }
+        public object[] errors { get; set; }
+        public int results { get; set; }
+        public Response[] response { get; set; }
+
+        public static async Task<NBAAPIResponse> GetNBATeam(string input)
+        {
+            NBAAPIResponse newbody = new NBAAPIResponse();
+
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api-nba-v1.p.rapidapi.com/"+input),
+                Headers =
+        {
+        { "X-RapidAPI-Key", "f05060df2fmshb5d970a07cc07bep17709djsnb951b9d040b3" },
+        { "X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com" },
+        },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                newbody = JsonSerializer.Deserialize<NBAAPIResponse>(body);
+                //Console.WriteLine(body);
+
+
+                return newbody;
+            }
+
+        }
+    }
+
+    public class Parameters
+    {
+        public string team { get; set; }
+        public string season { get; set; }
+
+    }
+
+    public class Response
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public Birth birth { get; set; }
+        public Nba nba { get; set; }
+        public Height height { get; set; }
+        public Weight weight { get; set; }
+        public string college { get; set; }
+        public string affiliation { get; set; }
+        public Leagues leagues { get; set; }
+
+
+    }
+
+    public class Birth
+    {
+        public string date { get; set; }
+        public string country { get; set; }
+    }
+
+    public class Nba
+    {
+        public int start { get; set; }
+        public int pro { get; set; }
+    }
+
+    public class Height
+    {
+        public string feets { get; set; }
+        public string inches { get; set; }
+        public string meters { get; set; }
+    }
+
+    public class Weight
+    {
+        public string pounds { get; set; }
+        public string kilograms { get; set; }
+    }
+
+    public class Leagues
+    {
+        public Standard standard { get; set; }
+        public Africa africa { get; set; }
+        public Vegas vegas { get; set; }
+        public Utah utah { get; set; }
+        public Sacramento sacramento { get; set; }
+    }
+
+    public class Standard
+    {
+        public int? jersey { get; set; }
+        public bool active { get; set; }
+        public string pos { get; set; }
+    }
+
+    public class Africa
+    {
+        public int? jersey { get; set; }
+        public bool active { get; set; }
+        public string pos { get; set; }
+    }
+
+    public class Vegas
+    {
+        public int jersey { get; set; }
+        public bool active { get; set; }
+        public string pos { get; set; }
+    }
+
+    public class Utah
+    {
+        public int jersey { get; set; }
+        public bool active { get; set; }
+        public string pos { get; set; }
+    }
+
+    public class Sacramento
+    {
+        public int jersey { get; set; }
+        public bool active { get; set; }
+        public string pos { get; set; }
+    }
+
+}
+
+
+    
